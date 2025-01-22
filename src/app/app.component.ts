d97533b9a259f0af997a7370200f5c6512db9a04
Luis Hernandez <luisadrianhdzsoto@gmail.com>
2025-01-22 14:49:41 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { MediaMatcher } from '@angular/cdk/layout';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'CG-Front';
+export class AppComponent implements OnInit, OnDestroy {
+
+  mobileQuery!: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor () {
+    const changeDetectorRef = inject(ChangeDetectorRef);
+    const media = inject(MediaMatcher);
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('../features/login/login.component').then(c => c.LoginComponent),
+    pathMatch: 'full' // Importante para evitar conflictos con rutas hijas
+  },
+  {
+    path: 'Entradas',
+    loadComponent: () => import('../features/main/main.component').then(c => c.MainComponent),
+    //canActivate:[AuthGuard] //Guarda para proteger la ruta
+  },
+  // {
+  //   path: 'inputs',
+  //   loadChildren: () => import('../features/inputs/inputs.module').then(m => m.InputsModule),
+  //   //canActivate:[AuthGuard] //Guarda para proteger la ruta
+  // },
+  //   {
+  //   path: 'admin',
+  //   loadChildren: () => import('../features/admin/admin.module').then(m => m.AdminModule),
+  //       //canActivate:[AuthGuard] //Guarda para proteger la ruta
+  // },
+  {
+    path: '',
+    redirectTo: 'login', // Redirige a /login por defecto
+    pathMatch: 'full'
+  },
+  {
+    path: '**', // Ruta comodín para manejar rutas no encontradas (404)
+    redirectTo: 'login' // Redirige a /login en caso de error
+  }
+];
